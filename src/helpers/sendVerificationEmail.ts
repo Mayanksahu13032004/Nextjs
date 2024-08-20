ec27d4c4f2941324e71c9ee58efc7541f332f540
Mayanksahu13032004 <mayanksahu13032004.com>
2024-08-20 21:06:31 +0530

    providers

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -4,7 +4,8 @@ import VerificationEmail from "../../emails/VerificationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
 
 export async function sendVerificationEmail(
-    email:string,
+    // ye tino aana he chaiya
+    email: string,
     username:string,
     verifyCode:string
 ):Promise<ApiResponse>{
@@ -15,7 +16,7 @@ export async function sendVerificationEmail(
             subject: 'Mystery message Verification code',
             react: VerificationEmail({username,otp:verifyCode}),
           });
-        return {success:true,message:'verification email successfully'}
+        return {success:true,message:'verification email send successfully'}
     } catch (emailError) {
         console.log("Error sending verification email",emailError);
         return {success:false,message:'Failed to send verification email'}
