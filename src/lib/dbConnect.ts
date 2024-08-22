5b8b6a5fb79fdf28a757aa795e8a5ce3baf0ee3d
Mayanksahu13032004 <mayanksahu13032004.com>
2024-08-22 22:22:36 +0530

    connection

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,35 +1,31 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-type ConnectionObject={
-    isConnected?:number
-    // ?matlab hogi yaa nahi hogi
-}
+type ConnectionObject = {
+  isConnected?: number;
+};
 
-const connection: ConnectionObject = {}
+const connection: ConnectionObject = {};
 
-async function dbConnect():Promise<void>{
-    if(connection.isConnected){
-        console.log("ALready connected to database");
-        return
-        
-    }
-    try{
-        const db= await mongoose.connect(process.env.MONGODB_URI || '',{})
-        connection.isConnected= db.connections[0].readyState
+async function dbConnect(): Promise<void> {
+  // Check if we have a connection to the database or if it's currently connecting
+  if (connection.isConnected) {
+    console.log('Already connected to the database');
+    return;
+  }
 
-        console.log("DB Connected successfully");
-        
-    }
-    catch(error){
-        console.log("Database connection failes!",error);
-        // Database is not connected than we exit 
-        process.exit(1)
-    }
+  try {
+    // Attempt to connect to the database
+    const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
+
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Database connection failed:');
+
+    // Graceful exit in case of a connection error
+    process.exit(1);
+  }
 }
 
 export default dbConnect;
-
-
-
-
-
