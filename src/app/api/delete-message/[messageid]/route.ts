76b434be631fe2adc04380dedb9e99db755efb1b
Mayanksahu13032004 <mayanksahu13032004.com>
2024-09-09 19:34:45 +0530

    add :complete project

---
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -0,0 +1,48 @@
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../../auth/[...nextauth]/options';
+import dbConnect from '@/lib/dbConnect';
+import UserModel from '@/model/User';
+import { User } from 'next-auth';
+import mongoose from 'mongoose';
+
+export async function DELETE(request: Request,{params}:{params:{messageid:string}}) {
+    const messageId= params.messageid
+    // Connect to the database
+
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User
+  
+  if (!session || !session.user) {
+    return Response.json(
+      { success: false, message: 'Not authenticated' },
+      { status: 401 }
+    );
+  }
+try {
+    const updateResult= await UserModel.updateOne(
+        {_id:user._id},
+        {$pull:{messages:{_id:messageId}}}
+    )
+    if(updateResult.modifiedCount==0){
+        return Response.json(
+            { success: false, message: 'Message not foud already deleted' },
+            { status: 404 }
+          );
+    }
+    return Response.json(
+        { success: true, message: 'Message deleted' },
+        { status: 401 }
+      );
+} catch (error) {
+    console.log("Error in deleye message route",error);
+    
+    return Response.json(
+        { success: false, message: 'Error deleting message' },
+        { status: 500 }
+      );
+}
+
+
+}
